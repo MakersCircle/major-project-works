c8d8871ea63fdbab755f4902cb32098516e1f848
Jacob Philip <jacobauxilium@gmail.com>
2025-02-03 11:42:12 +0530

    test

--- src/jacob/model.py
+++ src/jacob/model.py
@@ -4,3 +4,6 @@ def get_probabilities(frame_index):
     """Return a random probability for the given frame index."""
     np.random.seed(frame_index)  # Ensure consistent results for the same frame
     return np.random.rand()
+
+
+# sample
