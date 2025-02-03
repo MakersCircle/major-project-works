8e112e6a213490d5c836f7f61a757bf67099e05d
Jacob Philip <jacobauxilium@gmail.com>
2025-02-03 11:32:34 +0530

    Added visualisation modules

---
+++ src/jacob/model.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def get_probabilities(frame_index):
+    """Return a random probability for the given frame index."""
+    np.random.seed(frame_index)  # Ensure consistent results for the same frame
+    return np.random.rand()
